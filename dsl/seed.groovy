4f1389f46dd63b121b04c32b08a2f3d5c962a3cb
rahuldasdevops <46425314+rahuldasdevops@users.noreply.github.com>
2020-08-11 19:11:15 +0530

    Update seed.groovy

--- dsl/seed.groovy
+++ dsl/seed.groovy
@@ -1,6 +1,14 @@
 def createDeploymentJob(jobName) {
     pipelineJob(jobName) {
+    parameters {
+        stringParam('APP', '', 'Application name')
+        stringParam('EMAIL', 'admin@example.com', 'Associated email')
+        stringParam('PROJECT', 'None', 'Git project associated with application')
+        stringParam('GIT_REPO', 'None', 'The repository name to associate with application')
+    }
         definition {
+            cps{
+            }
         }
     }
 }
